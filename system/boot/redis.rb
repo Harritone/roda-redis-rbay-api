90c7f2202f9256b648c69cb2103d9c0e78feada5
Юрий Данилов <yuriy.ni.danilov@rt.ru>
2022-11-15 12:45:59 +0300

    init

---
+++ system/boot/redis.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+Application.boot(:redis) do |container|
+  # Load environment variables before setting up redis connection.
+  use :environment_variables
+
+  init do
+    require 'redis'
+  end
+
+  start do
+    # Define Redis instance.
+    redis = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'])
+
+    # Register redis component.
+    container.register(:redis, redis)
+  end
+end
